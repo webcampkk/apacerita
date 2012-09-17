e3045c1ce37767a96179717fb9ff3e5f1382363d
John Wong <john@flexnode.com>
2012-09-17 14:04:27 +0800

    Rails Skeleton

--- README.md
+++ README.md
@@ -1,4 +1,28 @@
-apacerita
-=========
+== Apacerita
 
-Event listing app bah
+Rails is a web-based prototype of a mobile application to gamify travel exploration
+
+
+== Requirements
+
+Ruby 1.9.3
+Rails 3.2.8
+MySQL 5.5
+
+== Getting Started
+
+bundle install
+bundle exec rake db:create
+bundle exec rake db:migrate
+bundle exec rails server
+
+== Dependencies 
+
+Refer to Gemfile
+
+
+== Credits
+
+WebcampKK Team :)
+
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Apacerita::Application.config.secret_token = 'b1e423aa5c306374094c95becc6bd014af9114679b30f527789ad3980886cc406c96497806f9383697dfa02862629aacc368cc9583c918b421973e02fc68afa4'
