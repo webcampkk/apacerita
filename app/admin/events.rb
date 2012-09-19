82a564786a3ce2dffeb5ab22ede311358d63765f
TS Lim <tslim@flexnode.com>
2012-09-19 14:23:41 +0800

    Made event form nicer in activeadmin

--- app/admin/events.rb
+++ app/admin/events.rb
@@ -12,4 +12,33 @@ ActiveAdmin.register Event do
     column :venue
     default_actions
   end
+
+  form do |f|
+    f.inputs "Basic Information" do
+      f.input :name
+      f.input :venue, :hint => "A short description for location of event. "
+      f.input :start_date
+      f.input :end_date
+      f.input :start_time, :as => :time
+      f.input :end_time, :as => :time
+      f.input :description
+    end
+
+    f.inputs "Location information" do
+      f.input :longitude
+      f.input :latitude
+    end
+
+    f.inputs "Contact Information" do
+      f.input :organizer
+      f.input :contact_person
+      f.input :phone_number
+      f.input :fax_number
+      f.input :website
+      f.input :email
+    end
+
+    f.buttons
+  end
+
 end
