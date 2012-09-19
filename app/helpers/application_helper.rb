4bac1a53d62f50d1cfba757cf0ac3fed2fd413dd
TS Lim <tslim@flexnode.com>
2012-09-19 16:04:09 +0800

    Wire up events index

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,24 @@
 module ApplicationHelper
+
+  def event_date_range(event)
+    date_range = ""
+    if event.start_date == event.end_date or event.end_date.blank?
+      date_range = "#{event.start_date.strftime("%d %B %Y")}"
+    elsif event.start_date.month == event.end_date.month && event.start_date.year == event.end_date.year
+      date_range = "#{event.start_date.strftime("%d")} - #{event.end_date.strftime("%d %b %Y")}"
+    else
+      date_range = "#{event.start_date.strftime("%d %b")} - #{event.end_date.strftime("%d %b %Y")}"
+    end  
+  end
+
+  def static_google_map_image(options = {})
+    options.reverse_merge!({ size: "250x200", scale: 1, sensor: false, zoom: 17 })
+    location = options.delete(:location)
+    location = location.join(",") if location.is_a?(Array)
+    url = "http://maps.googleapis.com/maps/api/staticmap?center=#{location}"
+    options.each { |k,v| url += "&#{k}=#{v}" }
+    url += "&markers=size:medium%7Ccolor:green%7C#{location}"
+    image_tag(url)
+  end
+
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -11,7 +11,34 @@ class Event < ActiveRecord::Base
 
   belongs_to :category
 
+  before_save :qualify_website_address, :set_date_for_time
+
   def date_and_time
+    str = "#{start_date.strftime("%d %b %Y")}"
+    str += " - #{end_date.strftime("%d %b %Y")}" unless end_date.blank?
+    str += " #{start_time.strftime("%I:%M %p")}" unless start_time.blank?
+    str += " #{end_time.strftime("%I:%M %p")}" unless end_time.blank?
+    str
+  end
+
+  def time
+    time_str = ""
+    time_str += "#{start_time.strftime("%I:%M %p")}" unless start_time.blank?
+    time_str += " - #{end_time.strftime("%I:%M %p")}" unless end_time.blank?
+  end
+
+  def location
+    return venue unless venue.blank?
+    return [latitude, longitude]
+  end
+
+protected
+  def qualify_website_address
+    return if website.blank?
+    self.website = "http://#{website}" unless website.starts_with?("http://")
+  end
+
+  def set_date_for_time
     
   end
 
