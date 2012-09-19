e6644c2407c426bfb1be417fe4159dd59895757d
TS Lim <tslim@flexnode.com>
2012-09-19 12:54:49 +0800

    Category id should be accessible in event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,7 +2,7 @@ class Event < ActiveRecord::Base
 
   attr_accessible :name, :venue, :start_date, :end_date, :start_time, :end_time,
                   :longitude, :latitude, :description, :organizer, :contact_person,
-                  :phone_number, :fax_number, :website, :email
+                  :phone_number, :fax_number, :website, :email, :category_id
 
   validates :name, :presence => true
   validates :venue, :presence => true
